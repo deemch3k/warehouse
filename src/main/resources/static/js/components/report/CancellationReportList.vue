4c7618fb3e2723478abbf4016fb00f1c0842fcc6
deemchek <smokeers3@gmail.com>
2020-10-25 18:51:22 +0100

    -adding serach for CancellationReportList.vue

--- src/main/resources/static/js/components/report/CancellationReportList.vue
+++ src/main/resources/static/js/components/report/CancellationReportList.vue
@@ -1,27 +1,41 @@
 <template>
-    <v-container>
-        <div v-for="report in sortedReports"
-             :key="report.id"
-             class="ma-5"
-        >
-           Order ID: {{report.orderId}}
-            <v-col></v-col>
-           Reason for Cancellation: {{report.reasonForCancellation}}
-            <v-divider></v-divider>
-        </div>
-    </v-container>
+  <v-container>
+    <div class="search-wrapper">
+      <v-text-field label="Search by order id" v-model="search"></v-text-field>
+    </div>
+    <div v-for="report in filteredList"
+         :key="report.id"
+         class="ma-5"
+    >
+      Order ID: {{ report.orderId }}
+      <v-col></v-col>
+      Reason for Cancellation: {{ report.reasonForCancellation }}
+      <v-divider></v-divider>
+    </div>
+  </v-container>
 </template>
 
 <script>
 
-    import {mapGetters} from 'vuex'
+import {mapGetters} from 'vuex'
 
-    export default {
-        name: 'CancellationReportList',
-        computed: {
-            ...mapGetters(['sortedReports']),
-        }
+export default {
+  name: 'CancellationReportList',
+
+  data() {
+    return {
+      search: ''
     }
+  },
+  computed: {
+    ...mapGetters(['sortedReports']),
+    filteredList() {
+      return this.sortedReports.filter(report => {
+        return report.orderId.includes(this.search)
+      })
+    }
+  }
+}
 
 </script>
 
